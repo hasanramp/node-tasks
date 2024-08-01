d252eb5dacc17c35abe78e47c9fa52c9838e3cd4
Hasan Rampurawala <rampurawalahasan4@gmail.com>
2024-08-01 20:41:55 +0530

    Added capital letters in array in task1.js

--- task1.js
+++ task1.js
@@ -5,7 +5,7 @@ const line_arr = line.split("");
 
 let unique_chars = [];
 let chars_map = new Map();
-let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 for (let i = 0; i < line_arr.length; i++) {
     if ( !unique_chars.includes(line_arr[i]) && letters.includes(line_arr[i])) {
